40bf6fcfd123af2ba55b9ad26a5ff0307f7d3830
almaan <almaan@kth.se>
2019-04-05 17:17:13 +0200

    spot plot and dge-res comp

--- main.r
+++ main.r
@@ -7,6 +7,7 @@ sh(library(org.Hs.eg.db))
 sh(library(AnnotationDbi))
 sh(library(ggplot2))
 sh(library(reshape2))
+sh((library(futile.logger)))
 # Functions -----------------------------
 
 ensg_to_rgb <- function(ensgid) {
@@ -23,6 +24,10 @@ ensg_to_rgb <- function(ensgid) {
   bb <- ((num / 7) %% 255 ) / 255
   rgbval <- rgb(r = rr, g = gg, b = bb)
   return(rgbval)
+}
+
+glue <- function(x) {
+  return(paste(x,collapse = ' '))
 }
 
 # Wrapper for multiple argument parsing -----------------
@@ -83,90 +88,141 @@ parser <- OptionParser()
 parser <- add_option(parser,
                      c("-d","--dge_res"),
                      default = NULL,
-                     help =""
+                     help =glue(c("Name of DGE-analysis results",
+                                  "file. Can be used as an alternative",
+                                  "to specifying specific genes"))
 )
 
-parser <- add_option(parser, c("-g","--genes",
+parser <- add_option(parser, c("-g","--genes"),
                                default = NULL,
-                               help = ""
-                               )
+                               help = glue(c("name of specfifc genes",
+                                             "to be visualized.",
+                                             "Is case insensitive"))
 )
 
 
 parser <- add_option(parser,
                      c("-c","--count_files"),
                      default = NULL,
-                     help = ""
+                     help = glue(c("path to count files.",
+                                   "order should match that",
+                                   "of feature file(s).",
+                                   "supports globbing."
+                                   ))
 )
 
 parser <- add_option(parser,
                      c("-f","--feature_files"),
                      default = NULL,
-                     help = ""
+                     help = glue(c("path to feature files.",
+                                   "order should match that",
+                                   "of count file(s)",
+                                   "supports globbing."
+                     ))
 )
 
 parser <- add_option(parser,
                      c("-p","--polydeg"),
                      default = 5,
-                     help = ""
+                     help = glue(c("degree of polynomial",
+                                   "to be used for interpolation",
+                                   "if method 'polynomial' is used"
+                                   ))
 )
 
 parser <- add_option(parser,
                      c("-t","--title"),
                      default = NULL,
-                     help = ""
+                     help = glue(c('title of plot',
+                                   'if non specified an automated',
+                                   'title will be generated'
+                                   ))
 )
 
 parser <- add_option(parser,
                      c("-r","--inner_tag"),
                      default = 'tumor',
-                     help = ""
+                     help = glue(c("name of variable to be",
+                                   "used as inner region.",
+                                   "Default is tumor. Will have",
+                                   "negative distance values"))
 )
 
 parser <- add_option(parser,
                      c("-x","--outer_tag"),
                      default = 'non',
-                     help = ""
+                     help = glue(c("name of variable to be",
+                                   "used as inner region.",
+                                   "Default is non as in non-tumor",
+                                   "Will have positive distance",
+                                   "values."
+                                   ))
 )
 
 parser <- add_option(parser,
                      c("-m","--method"),
                      default = 'polynomial',
-                     help = ""
+                     help = glue(c("method to use for the",
+                                   "curve fitting. Available",
+                                   "methods are 'polynomial'",
+                                   "(Default) and loess."))
 )
 
 parser <- add_option(parser,
                      c("-u","--positive_lfc"),
                      default = F,
                      action = 'store_true',
-                     help = ""
+                     help = glue(c("use flag to depict genes",
+                                   "with a positive logFoldChange",
+                                   "These will have red values in plot."))
 )
 
 parser <- add_option(parser,
                      c("-n","--negative_lfc"),
                      default = F,
                      action = 'store_true',
-                     help = ""
+                     help = glue(c("use flag to depict genes",
+                                   "with a negative logFoldChange",
+                                   "These will have blue values in plot."))
 )
 
 parser <- add_option(parser,
                      c("-s","--loess_span"),
                      default = 0.5,
-                     help = ""
+                     help = glue(c("span to use",
+                                   "in loess smoothing."))
 )
 
 parser <- add_option(parser,
                      c("-z","--z_transform"),
                      default = F,
                      action = 'store_true',
-                     help = ""
+                     help = glue(c("specify if z-transform of",
+                                   "values should be performed.",
+                                   "REMAIN TO BE IMPLEMENTED."))
+)
+
+parser <- add_option(parser,
+                     c("-e","--standard_error"),
+                     default = F,
+                     action = 'store_true',
+                     help = glue(c("Visualize standard error",
+                                    "in the resulting plots."))
+)
+
+parser <- add_option(parser,
+                     c("-q","--no_control"),
+                     default = T,
+                     action = 'store_false',
+                     help = glue(c("Do not control for matching",
+                                   "feature files and count files order",
+                                   "not recommended to use."))
 )
 
 # Variables -----------------
 
 if (interactive()) {
   # example set when running in interactive mode to test performance
-  # ajdust paths to make compatible with local version
   
   st_cnt_files <- c('exdata/st-00001_X1.tsv',
                     'exdata/st-00001_X2.tsv',
@@ -187,6 +243,9 @@ if (interactive()) {
   main_title <- "test"
   z_transform <- F
   use_genes <- c("ERBB2","MZB1")
+  use_standard_error <- T
+  use_se <- F
+  use_control <- TRUE
 
 } else {
   # For CLI usage
@@ -205,36 +264,42 @@ if (interactive()) {
   negative_lfc <- args$negative_lfc
   z_transform <- args$z_transform
   use_genes <- args$genes
+  use_se <- args$standard_error
+  use_control <- args$no_control
 }
 
 # Validate input -----------------------
 
 # check for consitency in number of files
 if(!(length(st_cnt_files) == length(feat_files))) {
-  print('Unequal number of count and feature files')
+  flog.error('The number of count matrices does not match
+             the number of feature files. Exiting.')
   quit(status = 1)
 }
 
 # makes sure count and feature fiels are matched
-for (filenum in c(1:length(feat_files))) {
-  pat <- "[0-9]{5}_[A-Z][0-9]" # section id pattern
-  # grep for section specific pattern in filenames
-  frgx <- regexpr(pat,feat_files[filenum],perl =T)
-  crgx <- regexpr(pat,st_cnt_files[filenum],perl =T)
-  # extract match
-  len <- attr(frgx, 'match.length')
-  fsec <- substr(feat_files[filenum],frgx[1],frgx[1] + len)
-  csec <- substr(st_cnt_files[filenum],crgx[1],crgx[1] + len)
-  
-  # exit if count and feature file section id does not match  
-  if (!(fsec == csec)) {
-    print('Not properly mathced files')
-    quit(status =1)
-  }
+if (use_control) {
+flog.info('Control for matching feature files and count files')
+  for (filenum in c(1:length(feat_files))) {
+      pat <- "[0-9]{5}_[A-Z][0-9]" # section id pattern
+      # grep for section specific pattern in filenames
+      frgx <- regexpr(pat,feat_files[filenum],perl =T)
+      crgx <- regexpr(pat,st_cnt_files[filenum],perl =T)
+      # extract match
+      len <- attr(frgx, 'match.length')
+      fsec <- substr(feat_files[filenum],frgx[1],frgx[1] + len)
+      csec <- substr(st_cnt_files[filenum],crgx[1],crgx[1] + len)
+      
+      # exit if count and feature file section id does not match  
+      if (!(fsec == csec)) {
+        log.error('Feature Files and Count Files are not properly matched')
+        quit(status =1)
+      }
+    }
 }
 
-print('>> Matching feature files and count files')
-print('>> Will Initiate analysis')
+flog.info('Matching feature files and count files')
+flog.info('Will Initiate analysis')
 
 # Main -----------------------
 analysis_type <- ifelse(z_transform,'z_transform','relative.freqs')
@@ -242,14 +307,13 @@ keepgenes <- c()
 cmap <- c()
 
 if (!(is.null(dge_pth))) {
-  print('using DGE-genes')
+  flog.info('Use Gene-Set and foldChange based on DE-results')
   # load genes to be used in analysis 
   genes <- read.table(dge_pth, sep = ',',
                       header =1,
                       row.names = 1,
                       stringsAsFactors = F)
   
-
   # if genes with positive lFC should be analyzed
   if (positive_lfc) {
     # get index of up-regulated genes
@@ -277,17 +341,16 @@ if (!(is.null(dge_pth))) {
   }
   
   if (!(negative_lfc) & !(positive_lfc)) {
-    print('>> Specify at least one set of genes to plot (lFC >0 or lFC <0')
-    print('>> Exiting')
+    flog.error('Specify at least one set of genes to plot (lFC >0 or lFC <0')
+    flog.error('Exiting')
     quit(status = 1)
+  }
   
   # name of analysis
   bname <- gsub("\\.tsv","",basename(genes_pth))
   # save result to png-file
   imgpth <- paste(c(dirname(genes_pth),paste(c(bname,analysis_type,"count_by_distance.png"),collapse = '.')),collapse = '/')
-    
-  
-  }
+  flog.info(sprintf('Will save results to >> %s',imgpth))
     
   genes <- genes[keepgenes,]
   genes <- genes$genes # get ENSEMBL ids
@@ -310,6 +373,8 @@ if (!(is.null(dge_pth))) {
 } else if (!(is.null(use_genes))) {
   genes <- use_genes
   
+  n_pre <- length(use_genes)
+  flog.info('Convert Gene Symbols to ENSEMBL Id')
   genes <-mapIds(org.Hs.eg.db,
           keys=as.character(genes),
           column="ENSEMBL",
@@ -319,7 +384,11 @@ if (!(is.null(dge_pth))) {
   symbol <- use_genes
   genes <- genes[!(is.na(genes))]
   symbol <- symbol[!(is.na(genes))]
+
+  n_post <- length(genes)
   
+  flog.info(sprintf("Lost %d/%d genes when converting between gene id's ", n_pre - n_post,n_pre))
+    
   grgb <- unlist(lapply(genes,ensg_to_rgb))
   cmap <- c(cmap,grgb)
   
@@ -328,12 +397,8 @@ if (!(is.null(dge_pth))) {
     dir.create(odir)
   }
   bname <- paste(c("custom_geneset",gsub(' |:|-','',as.character(Sys.time()))), collapse = "_")
-  imgpth <- paste(c(odir,paste(c(bname,analysis_type,"count_by_distance.png"),collapse = '.')),collapse = '/')
+  imgpth <- paste(c(odir,paste(c(bname,analysis_type),collapse = '.')),collapse = '/')
 }
-
-
-print(sprintf('>> The following genes %d genes',length(genes)))
-print(genes)
 
 mx <- 50 #  maximum distance from distance from tumor spot
 dr <- 0.1 #  distance increment
@@ -342,15 +407,30 @@ meanpnt <- lims + dr/2.0 #  mean point within each interval
 
 # dataframe to hold mean expression values for each distance value
 datanames <- c(rev(as.character(-meanpnt[-1])),as.character(meanpnt))
-data <- data.frame(matrix(0,ncol = length(genes), nrow = length(datanames)),
+spotcount <- data.frame(matrix(0,
+                               nrow = length(datanames)
+                               ),
+                        row.names = datanames
+                        )
+
+data <- data.frame(matrix(0,
+                          ncol = length(genes),
+                          nrow = length(datanames)
+                          ),
                    row.names = datanames)
+colnames(spotcount) <- 'n_spots'
 colnames(data) <- genes
 
 # iterate over all files
+flog.info('Read data from files')
 for (filenum in c(1:length(st_cnt_files))){
+  
   
   st_cnt_pth <- st_cnt_files[filenum]
   feat_pth <- feat_files[filenum]
+  
+  flog.info(sprintf('Using st-count file >> %s',st_cnt_pth))
+  flog.info(sprintf('Usign feature file >> %s',feat_pth))
   
   feat <- read.table(feat_pth, sep = '\t', header = 1, row.names = 1, stringsAsFactors = F)
   
@@ -371,10 +451,13 @@ for (filenum in c(1:length(st_cnt_files))){
   
   # interection of genelist and genes in st-count data
   if (sum(grepl('ENSG',colnames(cnt_raw))) > 0.5 * ncol(cnt_raw)) {
+    flog.info("Assuming Count Matrix uses ENSEMBL id's")
     inter <- as.character(intersect(genes,colnames(cnt_raw)))
   } else {
+    flog.info("Assuming Count Matrix uses Gene Symbols")
     inter <- as.character(intersect(symbols,colnames(cnt_raw)))
   }
+  
   # fill count matrix with scaled st-counts
   cnt[,inter] <- cnt_raw[,inter]
   remove(cnt_raw) # to free up space
@@ -397,11 +480,12 @@ for (filenum in c(1:length(st_cnt_files))){
     # find spots with mindist within I = [lim,lim+dr)
     idx_within_ref <- (mindist_ref >= lims[jj]) & (mindist_ref < lims[jj] + dr) 
     # get count matrix index for spots within I = [lim,lim+dr)
-    spt_within_ref <- which(rownames(cnt) %in% colnames(dmat)[idx_within_ref]) #  
+    spt_within_ref <- which(rownames(cnt) %in% colnames(dmat)[idx_within_ref])
+    spotcount[length(lims)-jj,] <- spotcount[length(lims)-jj,] + sum(idx_within_ref)
     # if I is non-empty
     if (length(spt_within_ref) > 0) {
       # add mean of observed counts taken over points within I
-      data[(length(lims) - jj),] <- sweep(data[length(lims) - jj,], FUN = '+', MARGIN = 2, colMeans(cnt[spt_within_ref,])) 
+      data[(length(lims) - jj),] <- data[length(lims) - jj,] + colMeans(cnt[spt_within_ref,]) 
     }
   }
   
@@ -411,48 +495,68 @@ for (filenum in c(1:length(st_cnt_files))){
     idx_within_trgt <- (mindist_trgt >= lims[ii]) & (mindist_trgt < lims[ii] + dr) 
     # get count matrix index for spots within I = [lim,lim+dr)
     spt_within_trgt <- which(rownames(cnt) %in% rownames(dmat)[idx_within_trgt]) #  
+    spotcount[(length(lims) + ii),] <-  spotcount[(length(lims) + ii),] + sum(idx_within_trgt)
     # if I is non-empty
     if (length(spt_within_trgt) > 0) {
       # add mean of observed counts taken over points within I
-      data[(length(lims) + ii),] <- sweep(data[(length(lims) + ii),], FUN = '+', MARGIN = 2, colMeans(cnt[spt_within_trgt,])) 
+      data[(length(lims) + ii),] <- data[(length(lims) + ii),] +  colMeans(cnt[spt_within_trgt,]) 
     }
   }
 
 }
+
 # adjust for multiple sections having been used
 dataf <- data / length(st_cnt_files)
 keepdist <- as.vector(which(rowSums(data) > 0, useNames = F))
+if (length(keepdist) == 0) {
+  flog.error('None of the selected genes were observed in any of the sections')
+  flog.error('Exiting')
+  
+}
+flog.info(sprintf('%d distances have at least one observed count of selected genes',length(keepdist)))
 # remove genes and distances where no observetions are made
 dataf <- dataf[keepdist,colSums(data) > 0]
+symbol <- symbol[colSums(data) >0]
+genes <- genes[colSums(data) >0]
 # remove distances where no spots are present
 meanpntf <- meanpnt[keepdist]
 datanamesf <- datanames[keepdist]
+spotcount$distance <- as.numeric(rownames(spotcount))
+spotcountf <- spotcount[keepdist,]
+
 # perform z-transform
 if (z_transform) {
+  flog.info('Z-transform data')
   dataf <- apply(dataf,2,scale)
 }
 
-png(imgpth,
+png(paste(c(imgpth,"count_by_distance.png"),collapse = '.'),
     width = 1000,
     height = 500)
 
 # get plot title
 if (is.null(main_title)) {
-  plt_title <- gsub('DGE_analysis\\.','',bname)
+  plt_title <- bname
 } else {
   plt_title <- main_title
 }
 
 # Plot Results -------------------------
 
+if (method == 'loess') {
+  flog.info(sprintf('Using Loess Smoothing with span %f', loess_span))
+  smooth_method <- loess
+  formula <- y ~ x
+} else {
+  flog.info(sprintf("Using Polynomial of degree %d for Smoothing",polydeg))
+  smooth_method <- lm
+  formula <-  y ~ poly(x, polydeg, raw=TRUE)  
+}
+
 cmx <- apply(dataf,2,max)
 cmn <- apply(dataf,2,min)
 dataf <- sweep(dataf, FUN = '-', MARGIN = 2, cmn)
 dataf <- sweep(dataf, FUN = '/', MARGIN = 2, (cmx-cmn))
-
-
-xmin <- min(as.numeric(datanamesf))
-xmax <- max(as.numeric(datanamesf))
 
 colnames(dataf) <- symbol
 dataf$average <- rowMeans(dataf)
@@ -467,7 +571,7 @@ viz <- ggplot(data = data_long, aes(x = distance, y = expression, color = gene))
        ggtitle(plt_title) +
        annotate('rect', xmin = -Inf, xmax = 0, ymin = -Inf, ymax = Inf, fill = "red", color = NA, alpha = 0.1) + 
        annotate('rect', xmin = 0, xmax = Inf, ymin = -Inf, ymax = Inf, fill = "green", color = NA, alpha = 0.1) +
-       geom_smooth(method=lm, alpha = 0.2,formula = y ~ poly(x, polydeg, raw=TRUE)) +
+       geom_smooth(method=smooth_method, se = use_se, alpha = 0.2,formula = formula, span = loess_span) +
        geom_point()  +
        scale_color_manual(values=cmap) +
        xlab('Distance') +
@@ -477,3 +581,22 @@ viz <- ggplot(data = data_long, aes(x = distance, y = expression, color = gene))
 print(viz)
 
 dev.off()
+png(paste(c(imgpth,"spot_distribution.png"),collapse = '.'),
+    width = 1000,
+    height = 500)
+
+bp <- ggplot(data = spotcountf, aes(x = as.factor(distance))) +
+    ggtitle(plt_title) +
+    geom_bar(aes(y = n_spots, fill = n_spots),stat = 'identity', color = 'black' ) +
+    scale_fill_gradient(low = "#966d2a",
+                        high = "#ffc463",
+                        space = "Lab",
+                        na.value = "grey50",
+                        aesthetics = "fill") +
+     xlab('Distance') +
+     ylab('Number of spots') +
+     theme(plot.title = element_text(hjust = 0.5))
+
+print(bp)
+dev.off()
+
